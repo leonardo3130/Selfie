646e149d46f0d58e585ccb4c9b393dc734463e84
leonardo3130 <leonardopo313@gmail.com>
2024-08-12 15:02:57 +0200

    project init

---
+++ server/dist/server.js
@@ -0,0 +1,17 @@
+//sintassi import anche per node invece di require, se volete cambio 
+//import: da un file .ts , nomeFile.js e non ts  
+//se invece per l'export usate una sintassi node: module.exports 
+//il file va incluso con estensione .cjs
+import express from 'express';
+// Initialize the Express application
+const app = express();
+// Define a basic route
+app.get('/', (req, res) => {
+    res.send('Hello, TypeScript with ES2020 modules!');
+});
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+//# sourceMappingURL=server.js.map
