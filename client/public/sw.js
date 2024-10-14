088bafb4816052cec1ee52391cc630c1c099befe
leonardo3130 <leonardopo313@gmail.com>
2024-10-14 22:14:30 +0200

    add intervalTimer per controllo notifiche

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,7 +1,6 @@
-
 //per ora notifica semplice (quelle del calendario)
-self.addEventListener('push', (event) => {
-  console.log('Push event received:', event);
+self.addEventListener("push", (event) => {
+  console.log("Push event received:", event);
   const data = event.data.json();
   console.log(data.message);
 
@@ -13,7 +12,32 @@ self.addEventListener('push', (event) => {
   //
   event.waitUntil(
     // self.registration.showNotification(data.title, options)
-    self.registration.showNotification(data.message)
+    self.registration.showNotification(data.message),
   );
 });
-
+//
+// // DA RIVEDERE , importante per pomodoro
+// self.addEventListener("notificationclick", function (event) {
+//   console.log("Notification click received");
+//   event.notification.close();
+//   // This looks to see if the current is already open and
+//   // focuses if it is
+//   event.waitUntil(
+//     clients
+//       .matchAll({
+//         type: "window",
+//         includeUncontrolled: true,
+//       })
+//       .then(function (clientList) {
+//         for (var i = 0; i < clientList.length; i++) {
+//           var client = clientList[i];
+//           if (client.url === "/" && "focus" in client) {
+//             return client.focus();
+//           }
+//         }
+//         if (clients.openWindow) {
+//           return clients.openWindow("/");
+//         }
+//       }),
+//   );
+// });
--- server/src/utils/notificationDaemon.ts
+++ server/src/utils/notificationDaemon.ts
@@ -1,1 +1,98 @@
 //function running to check moment for sending notifications
+import webpush from "web-push";
+import schedule from "node-schedule";
+import { EventModel, IEvent } from "../models/eventModel.js";
+import { UserModel, IUser } from "../models/userModel.js";
+
+const MILLIS_IN_DAY = 1000 * 60 * 60 * 24;
+
+function getMilllisecondsUntilMidnight() {
+  const now = new Date();
+  const nextDay = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+  return nextDay.getTime() - now.getTime();
+}
+
+//TODO: schedulare notifiche
+//TODO: interazione con time_machine, capire bene come collegare con altre routes + rescheduling
+//TODO: notifiche mail
+//TODO: controllo query
+
+//esecuzione sempre a mezzanotte
+setTimeout(() => {
+  setInterval( async () => {
+    const eventToUserMap = new Map<IEvent, IUser[]>();
+    //check for today notifications
+    //prendo gli eventi di oggi
+    const fiveDaysMs = MILLIS_IN_DAY * 5;
+    //le notifiche possono arrivare fino a 5 giotni prima
+    //quindi prendo tutti gli eventi fino a 5 giorni prima da ora
+    const events: IEvent[] | null = await EventModel.find({ 
+      date: {
+        //qui uso data corrente, ma sarà data time machine
+        $gte: new Date(new Date().setDate(new Date().getDate() - fiveDaysMs)),
+        $lt: new Date(new Date().setDate(new Date().getDate() + fiveDaysMs))
+      },
+      notifications: {
+        $or: [
+          {
+            $elemMatch: {
+              notifica_desktop: true,
+            }
+          },
+          {
+            $elemMatch: {
+              notifica_email: true,
+            }
+          }
+        ]
+      }
+    }); //da sistemare
+    //prendo tutti gli utenti
+    for (const event of events) {
+      const users: IUser[] = [];
+      //utenti proprietari di eventi che hanno acconsentito all'invio di notifiche
+      const user: IUser | null = await UserModel.findOne({
+        _id: event._id_user,
+        flags: { 
+          $or: [
+            { 'flags.notifica_desktop': true }, 
+            { 'flags.notifica_email': true } 
+          ] 
+        },
+      });
+      //utenti partecipanti di eventi che hanno acconsentito all'invio di notifiche
+      const otherUsers: IUser[] | null = await UserModel.find({
+        _id: { $in: event.attendees },
+        flags: { 
+          $or: [
+            { 'flags.notifica_desktop': true }, 
+            { 'flags.notifica_email': true } 
+          ] 
+        },
+      });
+      if(user)
+        users.push(user);
+      if(otherUsers)
+        users.push(...otherUsers);
+      eventToUserMap.set(event, users);
+    }
+    //qui ho tutti gli eventi e tutti gli utenti coinvolti
+    //per ogni notifica, invio una notifica per ogni utente
+    eventToUserMap.forEach(async (users: IUser[], event: IEvent) => {
+      for (const user of users) {
+        //notifica per email
+        if (user.flags.notifica_desktop) {
+          //notifica per email
+          for (const sub of user.pushSubscriptions) {
+            //QUI NON MANDO SUBITO, MA FACCIO SCHEDULING
+            webpush.sendNotification(sub, JSON.stringify({
+              title: "Selfie",
+              body: event.title,
+              url: event.url,
+            }));
+          }
+        }
+      }
+    })
+  }, MILLIS_IN_DAY);
+}, getMilllisecondsUntilMidnight());
