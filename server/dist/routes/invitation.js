044d6dbb2c32c5b313e17b3c0a373b6bec3f1312
leonardo3130 <leonardopo313@gmail.com>
2025-04-01 02:03:27 +0200

    removed dist and node modules

--- server/dist/routes/invitation.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { acceptActivityInvitation, acceptEventInvitation } from "../controllers/invitationController.js";
-import { requireAuth } from "../middleware/requireAuth.js";
-const invitationRoutes = express.Router();
-invitationRoutes.use(requireAuth);
-invitationRoutes.patch("/activity/:id", acceptActivityInvitation);
-invitationRoutes.patch("/event/:id", acceptEventInvitation);
-export { invitationRoutes };
-//# sourceMappingURL=invitation.js.map
--- server/dist/routes/message.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getMessages, sendMessage } from '../controllers/messagesController.js';
-import { requireAuth } from '../middleware/requireAuth.js';
-const messageRoutes = express.Router();
-messageRoutes.use(requireAuth);
-messageRoutes.get('/get/:id', getMessages);
-messageRoutes.post('/send/:sender_id', sendMessage);
-export { messageRoutes };
-//# sourceMappingURL=message.js.map
